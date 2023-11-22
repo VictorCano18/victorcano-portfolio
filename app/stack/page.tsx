e6718decd9fa4419f7a6e53c14e33f43c897ed63
VictorCano18 <vikkno123@gmail.com>
2023-11-21 23:11:51 -0600

    First 2 pages

---
+++ app/stack/page.tsx
@@ -0,0 +1,42 @@
+export default function stack() {
+    return (
+        <div className="container mx-auto flex justify-center align-items-center flex-col">
+            <h1 className="text-yellow-600">Stack</h1>
+            <div className="flex gap-10">
+                <div className="text-white">
+                    <h1 className="text-red-600">Frontend</h1>
+                    <div>Javascript</div>
+                    <div>React</div>
+                    <div>Bootstrap</div>
+                    <div>TailwindCSS</div>
+                    <div>Angular</div>
+                    <div>JQuery</div>
+                    <div>Material-UI</div>
+                    <div>SASS</div>
+                </div>
+                <div className="text-white">
+                    <h1 className="text-red-600">Backend</h1>
+                    <div>Node JS</div>
+                    <div>Python</div>
+                    <div>AWS</div>
+                    <div>Express</div>
+                </div>
+                <div className="text-white">
+                    <h1 className="text-red-600">Databases</h1>
+                    <div>MongoDB</div>
+                    <div>MySQL</div>
+                </div>
+                <div className="text-white">
+                    <h1 className="text-red-600">More stack</h1>
+                    <div>Java</div>
+                    <div>Springboot</div>
+                    <div>C</div>
+                    <div>C#</div>
+                    <div>C++</div>
+                    <div>Unity</div>
+                    <div>Vue</div>
+                </div>
+            </div>
+        </div>
+    )
+}
