b24ae204ae50a7106143fe4027674b5f219c6d68
VictorCano18 <vikkno123@gmail.com>
2023-12-04 09:25:12 -0600

    remove anchor tags

--- app/header/page.tsx
+++ app/header/page.tsx
@@ -46,32 +46,24 @@ const Header: React.FC = () => {
               <div className="text-xl text-gray-500 dark:text-gray-400">Apassionate software engineer focused on web development and minimalist design</div>
               <div className='flex flex-row gap-2 sm:justify-start justify-center sm:items-start items-center pt-2'>
                 <Tooltip content="Mail me" className='text-black bg-yellow-500 rounded'>
-                  <a href='#section5' className=''>
-                    <Button pill className='w-fit animationIcon'>
-                      <IoIosMail className='text-xl animateIcon' />
-                    </Button>
-                  </a>
+                  <Button href='#section5' pill className='w-fit animationIcon'>
+                    <IoIosMail className='text-xl animateIcon' />
+                  </Button>
                 </Tooltip>
                 <Tooltip content="GitHub source" className='text-black bg-yellow-500 rounded'>
-                  <a href='https://github.com/VictorCano18/victorcano-portfolio' target='_blank'>
-                    <Button pill className='w-fit animationIcon' color="purple">
-                      <FaGithub className='text-xl animateIcon' />
-                    </Button>
-                  </a>
+                  <Button href='https://github.com/VictorCano18/victorcano-portfolio' target='_blank' pill className='w-fit animationIcon' color="purple">
+                    <FaGithub className='text-xl animateIcon' />
+                  </Button>
                 </Tooltip>
                 <Tooltip content="LinkedIn" className='text-black bg-yellow-500 rounded'>
-                  <a href="https://www.linkedin.com/in/vics1806/" target='_blank'>
-                    <Button pill className='w-fit animationIcon' color="blue">
-                      <FaLinkedin className='text-xl animateIcon' />
-                    </Button>
-                  </a>
+                  <Button href="https://www.linkedin.com/in/vics1806/" target='_blank' pill className='w-fit animationIcon' color="blue">
+                    <FaLinkedin className='text-xl animateIcon' />
+                  </Button>
                 </Tooltip>
                 <Tooltip content="Resume" className='text-black bg-yellow-500 rounded'>
-                  <a>
-                    <Button onClick={handleDownload} pill className='w-fit animationIcon' color="failure">
-                      <FaFileInvoice className='text-xl animateIcon' />
-                    </Button>
-                  </a>
+                  <Button onClick={handleDownload} pill className='w-fit animationIcon' color="failure">
+                    <FaFileInvoice className='text-xl animateIcon' />
+                  </Button>
                 </Tooltip>
               </div>
             </div>
