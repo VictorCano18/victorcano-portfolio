706deb70f5c989a5ced84e5a7615e88aef94c9b6
Victor Cano <victorcano@Victors-MacBook-Pro.local>
2024-06-26 22:31:02 -0600

    feat(main): add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import { Metadata } from 'next';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: 'Victor Cano Portfolio',
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className='bg-black sm:max-w-screen-sm mx-auto relative px-5'>{children}</body>
+      <GoogleAnalytics gaId="G-8JG0BDHZ7T" />
     </html>
   );
 }
