08ca55abac7b165b586c9ab529d35d6fcf400f96
Victor Cano <vikkno123@gmail.com>
2024-11-30 14:57:52 -0600

    feat(master): add new bella nails project

--- app/pages/projects/page.tsx
+++ app/pages/projects/page.tsx
@@ -1,39 +1,105 @@
-"use client"; // This is a client component
+'use client'; // This is a client component
 
 import { Button, Tooltip } from 'flowbite-react';
 import React from 'react';
-import worldRank from '../../../public/assets/world_rank_project.png'
-import { RiShareBoxLine } from "react-icons/ri";
-import { FaGithub } from "react-icons/fa";
-
-
+import worldRank from '../../../public/assets/world_rank_project.png';
+import bellaNails from '../../../public/assets/bella_nails_project.png'
+import { RiShareBoxLine } from 'react-icons/ri';
+import { FaGithub } from 'react-icons/fa';
 
 export default function Projects() {
-
-    return (
-        <div className="text-white relative sm:max-w-full max-w-xs mx-auto">
-            <h1 className="text-yellow-600 text-4xl sm:text-5xl mb-5">Projects</h1>
-            <div className='relative flex flex-col sm:flex-row items-center gap-6'>
-                <img src={worldRank.src} className='sm:w-1/2 w-fit rounded-xl border border-gray-700' alt='world-rank' />
-                <div className='flex flex-col gap-2'>
-                    <div className='flex flex-col items-center justify-center text-center'>
-                        <div className='text-2xl strong dateColor'>World Rank</div>
-                        <p>Project made with <strong>React + Vite.js</strong> and for styling <strong>SCSS</strong></p>
-                    </div>
-                    <div className='flex gap-2 items-center justify-center'>
-                        <Tooltip content="GitHub source" className='text-black bg-yellow-500 rounded text-yellow-400'>
-                            <Button href='https://github.com/VictorCano18/world-rank' target='_blank' pill className='w-fit animationIcon' color="purple">
-                                <FaGithub className='text-xl animateIcon' />
-                            </Button>
-                        </Tooltip>
-                        <Tooltip content="Project" className='text-black bg-yellow-500 rounded text-yellow-400'>
-                            <Button href='https://world-rank-liard.vercel.app/' pill className='w-fit animationIcon'>
-                                <RiShareBoxLine className='text-xl animateIcon' />
-                            </Button>
-                        </Tooltip>
-                    </div>
-                </div>
+  return (
+    <div className="relative mx-auto max-w-xs text-white sm:max-w-full">
+      <h1 className="mb-5 text-4xl text-yellow-600 sm:text-5xl">Projects</h1>
+      <div className='flex flex-col items-center gap-16'>
+        <div className="relative flex flex-col items-center gap-6 sm:flex-row">
+          <img
+            src={worldRank.src}
+            className="w-fit rounded-xl border border-gray-700 sm:w-1/2"
+            alt="world-rank"
+          />
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col items-center justify-center text-center">
+              <div className="strong dateColor text-2xl">World Rank</div>
+              <p>
+                Project made with <strong>React + Vite.js</strong> and for
+                styling <strong>SCSS</strong>
+              </p>
             </div>
+            <div className="flex items-center justify-center gap-2">
+              <Tooltip
+                content="GitHub source"
+                className="rounded bg-yellow-500 text-black text-yellow-400"
+              >
+                <Button
+                  href="https://github.com/VictorCano18/world-rank"
+                  target="_blank"
+                  pill
+                  className="animationIcon w-fit"
+                  color="purple"
+                >
+                  <FaGithub className="animateIcon text-xl" />
+                </Button>
+              </Tooltip>
+              <Tooltip
+                content="Project"
+                className="rounded bg-yellow-500 text-black text-yellow-400"
+              >
+                <Button
+                  href="https://world-rank-liard.vercel.app/"
+                  pill
+                  className="animationIcon w-fit"
+                >
+                  <RiShareBoxLine className="animateIcon text-xl" />
+                </Button>
+              </Tooltip>
+            </div>
+          </div>
         </div>
-    );
+        <div className="relative flex flex-col items-center gap-6 sm:flex-row">
+          <img
+            src={bellaNails.src}
+            className="w-fit rounded-xl border border-gray-700 sm:w-1/2"
+            alt="world-rank"
+          />
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col items-center justify-center text-center">
+              <div className="strong dateColor text-2xl">Bella Nails</div>
+              <p>
+                Project made with <strong>React + Vite.js</strong> and for
+                styling <strong>Tailwind</strong>
+              </p>
+            </div>
+            <div className="flex items-center justify-center gap-2">
+              <Tooltip
+                content="GitHub source private"
+                className="rounded bg-red-500 text-black text-red-400"
+              >
+                <Button
+                  disabled
+                  pill
+                  className="animationIcon w-fit cursor"
+                  color="purple"
+                >
+                  <FaGithub className="animateIcon text-xl" />
+                </Button>
+              </Tooltip>
+              <Tooltip
+                content="Project"
+                className="rounded bg-yellow-500 text-black text-yellow-400"
+              >
+                <Button
+                  href="http://bella.nails.s3-website.us-east-2.amazonaws.com/"
+                  pill
+                  className="animationIcon w-fit"
+                >
+                  <RiShareBoxLine className="animateIcon text-xl" />
+                </Button>
+              </Tooltip>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
