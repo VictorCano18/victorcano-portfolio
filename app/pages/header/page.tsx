2d16e9f55d8e0ac7e2a113bf6d81bcb03cb5c23b
Victor Cano <vikkno123@gmail.com>
2025-01-26 21:20:55 -0600

    fix(master): fix svg on small sizes

--- app/pages/header/page.tsx
+++ app/pages/header/page.tsx
@@ -1,12 +1,12 @@
-"use client"; // This is a client component
+'use client'; // This is a client component
 
 import React, { useEffect, useState } from 'react';
 import { Avatar, Button } from 'flowbite-react';
-import AvatarImage from '../../../public/assets/avatar.svg'
-import { IoIosMail } from "react-icons/io";
-import { FaFileInvoice } from "react-icons/fa6";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { Tooltip } from "@nextui-org/react";
+import AvatarImage from '../../../public/assets/avatar.svg';
+import { IoIosMail } from 'react-icons/io';
+import { FaFileInvoice } from 'react-icons/fa6';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { Tooltip } from '@nextui-org/react';
 
 // Use the useClient pragma to mark the component for client-side rendering
 // @ts-ignore
@@ -38,53 +38,99 @@ const Header: React.FC = () => {
   }, []);
   return (
     <header>
-      <div className="flex flex-col main-heading">
-        <div className='flex flex-col sm:flex-row gap-4 justify-center items-center mb-2'>
-          <img src={AvatarImage.src} alt='avatar of me' className='sm:space-x-4 space-x-0 sm:w-2/5 w-3/5' />
-          <div className="text-5xl space-y-1 font-medium text-white dark:text-white text-center sm:text-left">
-            <div className='font-medium'>Víctor Cano</div>
-            <div className="text-xl text-gray-300 dark:text-gray-300">Apassionate software engineer focused on web development and minimalist design</div>
-            <div className='flex flex-row gap-2 sm:justify-start justify-center sm:items-start items-center pt-2'>
-              <Tooltip content="Mail me" className='text-black bg-yellow-500 rounded'>
-                <Button href='#section6' pill className='w-fit animationIcon' color="dark">
-                  <IoIosMail className='text-xl animateIcon' />
+      <div className="main-heading flex flex-col">
+        <div className="mb-2 flex flex-col items-center justify-center gap-4 sm:flex-row">
+          <img
+            src={AvatarImage.src}
+            alt="avatar of me"
+            className="h-3/5 w-3/5 sm:h-2/5 sm:w-2/5"
+          />
+          <div className="space-y-1 text-center text-5xl font-medium text-white dark:text-white sm:text-left">
+            <div className="font-medium">Víctor Cano</div>
+            <div className="text-xl text-gray-300 dark:text-gray-300">
+              Apassionate software engineer focused on web development and
+              minimalist design
+            </div>
+            <div className="flex flex-row items-center justify-center gap-2 pt-2 sm:items-start sm:justify-start">
+              <Tooltip
+                content="Mail me"
+                className="rounded bg-yellow-500 text-black"
+              >
+                <Button
+                  href="#section6"
+                  pill
+                  className="animationIcon w-fit"
+                  color="dark"
+                >
+                  <IoIosMail className="animateIcon text-xl" />
                 </Button>
               </Tooltip>
-              <Tooltip content="GitHub source" className='text-black bg-yellow-500 rounded'>
-                <Button href='https://github.com/VictorCano18/victorcano-portfolio' target='_blank' pill className='w-fit animationIcon' color="dark">
-                  <FaGithub className='text-xl animateIcon' />
+              <Tooltip
+                content="GitHub source"
+                className="rounded bg-yellow-500 text-black"
+              >
+                <Button
+                  href="https://github.com/VictorCano18/victorcano-portfolio"
+                  target="_blank"
+                  pill
+                  className="animationIcon w-fit"
+                  color="dark"
+                >
+                  <FaGithub className="animateIcon text-xl" />
                 </Button>
               </Tooltip>
-              <Tooltip content="LinkedIn" className='text-black bg-yellow-500 rounded'>
-                <Button href="https://www.linkedin.com/in/vics1806/" target='_blank' pill className='w-fit animationIcon' color="dark">
-                  <FaLinkedin className='text-xl animateIcon' />
+              <Tooltip
+                content="LinkedIn"
+                className="rounded bg-yellow-500 text-black"
+              >
+                <Button
+                  href="https://www.linkedin.com/in/vics1806/"
+                  target="_blank"
+                  pill
+                  className="animationIcon w-fit"
+                  color="dark"
+                >
+                  <FaLinkedin className="animateIcon text-xl" />
                 </Button>
               </Tooltip>
-              <Tooltip content="Resume" className='text-black bg-yellow-500 rounded'>
-                <Button onClick={handleDownload} pill className='w-fit animationIcon' color="dark">
-                  <FaFileInvoice className='text-xl animateIcon' />
+              <Tooltip
+                content="Resume"
+                className="rounded bg-yellow-500 text-black"
+              >
+                <Button
+                  onClick={handleDownload}
+                  pill
+                  className="animationIcon w-fit"
+                  color="dark"
+                >
+                  <FaFileInvoice className="animateIcon text-xl" />
                 </Button>
               </Tooltip>
             </div>
           </div>
         </div>
         <div className={`${!shouldHide && 'hidden'}`}>
-          <a href='#section1'>
-            <Avatar img={AvatarImage.src} rounded className='avatarMeHeader justify-start fixed sm:space-x-4 space-x-0 z-30 w-full xl:w-3/4 p-2 bg-black sm:gap-0 gap-5 top-0'>
-              <div className="sm:text-5xl text-3xl space-y-1 font-medium dark:text-white text-left">
+          <a href="#section1">
+            <Avatar
+              img={AvatarImage.src}
+              rounded
+              className="avatarMeHeader fixed top-0 z-30 w-full justify-start gap-5 space-x-0 bg-black p-2 sm:gap-0 sm:space-x-4 xl:w-3/4"
+            >
+              <div className="space-y-1 text-left text-3xl font-medium dark:text-white sm:text-5xl">
                 <div>Víctor Cano</div>
               </div>
             </Avatar>
           </a>
         </div>
-        <hr style={{
-          width: '100%',
-          marginTop: '1rem'
-        }} />
+        <hr
+          style={{
+            width: '100%',
+            marginTop: '1rem',
+          }}
+        />
       </div>
     </header>
   );
-}
+};
 
 export default Header;
-
